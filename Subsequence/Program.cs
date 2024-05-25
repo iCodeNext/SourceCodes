d9eb6f2a55126eaac396bf9f43d16d2524c5711d
mohammadKarimi <mha.karimi@gmail.com>
2024-05-25 12:29:09 +0330

    add subsequence

--- Subsequence/Program.cs
+++ Subsequence/Program.cs
@@ -1,91 +1,293 @@
 ﻿
+using System;
 
-
-
-using System.ComponentModel.DataAnnotations;
-
-class Result
+public class Solution
 {
-    public static List<int> getMaxSubsequenceLen(List<int> arr)
+    public static List<int> GetMaxSubsequenceLen(List<int> arr)
     {
-        List<int> result = [];
-        arr.Sort();
-        for (int i = 0; i < arr.Count; i++)
-            result.Add(GetLongestSubsequence(arr, arr[i], i));
-        return result;
-    }
+        List<int> sortedArray = [.. arr];
+        sortedArray.Sort();
 
-    private static int GetLongestSubsequence(List<int> arr, int median, int index)
-    {
-        List<int> lessThanMedian = [];
-        List<int> greaterThanMedian = [];
-        List<int> medianList = [];
-        for (int i = 0; i < arr.Count ; i++)
+        Dictionary<int, int> subsequenceLength = [];
+
+        for (int i = 0; i < sortedArray.Count; i++)
         {
-            if (i == index)
-                continue;
-            if (i < index && arr[i] < median)
-                lessThanMedian.Add(arr[i]);
-            else if (i > index && arr[i] > median)
-                greaterThanMedian.Add(arr[i]);
-        }
+            int medianValue = sortedArray[i];
+            List<int> leftSubArray = sortedArray.Take(i).ToList();
+            List<int> rightSubArray = sortedArray.Skip(i + 1).ToList();
+            List<int> median = arr.Where(x => x == medianValue).ToList();
 
-        int minimumDistinct = Math.Min(lessThanMedian.Distinct().Count(), greaterThanMedian.Distinct().Count());
-        int arrayLengthBalance = Math.Min(lessThanMedian.Count, greaterThanMedian.Count);
+            var leftDistinctCollection = new HashSet<int>(leftSubArray.Where(x => x != medianValue));
+            var rightDistinctCollection = new HashSet<int>(rightSubArray.Where(x => x != medianValue));
 
-
-        List<int> result = [.. GetSubsequenceList(lessThanMedian, arrayLengthBalance, minimumDistinct),
-            median,
-            .. GetSubsequenceList(greaterThanMedian, arrayLengthBalance, minimumDistinct)];
-
-
-        return result.Count % 2 == 0 ? result.Count - 1 : result.Count;
-    }
-
-    private static List<int> GetSubsequenceList(List<int> arr, int length, int minimumDistinct)
-    {
-        if (arr.Count == 0 || length == 0)
-            return [];
-
-
-        var frequencyDict = arr.GroupBy(n => n)
-                               .ToDictionary(g => g.Key, g => g.Count());
-
-        var topFrequentNumbers = frequencyDict.OrderByDescending(x => x.Value)
-                                              .ThenBy(x => arr.IndexOf(x.Key))
-                                              .Select(x => x.Key)
-                                              .Take(minimumDistinct)
-                                              .ToList();
-        List<int> result = topFrequentNumbers;
-
-
-
-        foreach (var item in arr)
-        {
-            if (result.Contains(item)
-                && frequencyDict[item] > result.Where(x => x == item).Count()
-                && result.Count < length)
+            if (rightDistinctCollection.Count == 0 || leftDistinctCollection.Count == 0)
             {
-                result.Add(item);
+                int length = median.Count % 2 == 0 ? median.Count - 1 : median.Count;
+                UpdateSubsequenceLength(subsequenceLength, sortedArray[i], length);
+            }
+            else if (leftSubArray.Count < rightSubArray.Count)
+            {
+                var leftDistinctCount = leftDistinctCollection.Count;
+                var balancedArray = FindBalanceSubcequence(rightSubArray, medianValue, leftDistinctCount, leftSubArray.Count);
+                if (balancedArray)
+                {
+                    var length = leftSubArray.Count * 2 + 1;
+                    length = length % 2 == 0 ? length - 1 : length;
+                    UpdateSubsequenceLength(subsequenceLength, sortedArray[i], length);
+                }
+                else
+                {
+                    var maxLeft = leftDistinctCount;
+                    while (maxLeft > 0)
+                    {
+                        maxLeft--;
+                        var allocation = CalculateMaxAllocation([.. leftDistinctCollection], maxLeft);
+                        balancedArray = FindBalanceSubcequence(rightSubArray, medianValue, maxLeft, allocation);
+                        if (balancedArray)
+                        {
+                            var length = allocation * 2 + 1;
+                            length = length % 2 == 0 ? length - 1 : length;
+                            UpdateSubsequenceLength(subsequenceLength, sortedArray[i], length);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var rightDistinctCount = rightDistinctCollection.Count;
+                var balancedArray = FindBalanceSubcequence(leftSubArray, medianValue, rightDistinctCount, rightSubArray.Count);
+                if (balancedArray)
+                {
+                    var length = rightSubArray.Count * 2 + 1;
+                    length = length % 2 == 0 ? length - 1 : length;
+                    UpdateSubsequenceLength(subsequenceLength, sortedArray[i], length);
+                }
+                else
+                {
+                    var maxright = rightDistinctCount;
+                    while (maxright > 0)
+                    {
+                        maxright--;
+                        var allocation = CalculateMaxAllocation([.. rightDistinctCollection], maxright);
+                        balancedArray = FindBalanceSubcequence(leftSubArray, medianValue, maxright, allocation);
+                        if (balancedArray)
+                        {
+                            var length = allocation * 2 + 1;
+                            length = length % 2 == 0 ? length - 1 : length;
+                            UpdateSubsequenceLength(subsequenceLength, sortedArray[i], length);
+                            break;
+                        }
+                    }
+                    if (rightDistinctCount == 1)
+                    {
+                        maxright = rightSubArray.Count;
+                        while (maxright > 0)
+                        {
+                            maxright--;
+                            balancedArray = FindBalanceSubcequence(leftSubArray, medianValue, rightDistinctCount, maxright);
+                            if (balancedArray)
+                            {
+                                var length = maxright * 2 + 1;
+                                length = length % 2 == 0 ? length - 1 : length;
+                                UpdateSubsequenceLength(subsequenceLength, sortedArray[i], length);
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        return result;
+        List<int> finalResult = arr.Select(item => subsequenceLength.ContainsKey(item) ? subsequenceLength[item] : 0).ToList();
+        return finalResult;
+
     }
-}
 
-//miolsoft
+    private static void UpdateSubsequenceLength(Dictionary<int, int> subsequenceLength, int key, int length)
+    {
+        if (subsequenceLength.TryGetValue(key, out int value))
+        {
+            if (value < length)
+                subsequenceLength[key] = length;
+        }
+        else
+            subsequenceLength.Add(key, length);
+    }
 
-class Solution
-{
-    public static void Main(string[] args)
+    private static int CalculateMaxAllocation(List<int> array, int distinctCount)
     {
 
 
-        List<int> result = Result.getMaxSubsequenceLen([1, 3, 2, 1, 3]);
+        var frequencyDict = array.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
 
-        // Console.WriteLine(String.Join("\n", result));
-        // Console.WriteLine();
-        Console.Read();
+        var allocation = frequencyDict.OrderByDescending(x => x.Value)
+                                              .Select(x => x.Key)
+                                              .Take(distinctCount)
+                                              .Sum();
+        return allocation;
+    }
+
+    private static bool FindBalanceSubcequence(List<int> array, int medianValue, int distinctCount, int length)
+    {
+        var countOfDistinct = array.Where(x => x != medianValue).Distinct().Count();
+        if (countOfDistinct < distinctCount)
+            return false;
+
+        var frequencyDict = array.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+
+        var topFrequentNumbers = frequencyDict.Where(x => x.Key != medianValue).OrderByDescending(x => x.Value)
+                                              .ThenBy(x => array.IndexOf(x.Key))
+                                              .Select(x => x.Key)
+                                              .Take(distinctCount)
+                                              .ToList();
+        var result = topFrequentNumbers;
+        foreach (var num in array)
+            if ((topFrequentNumbers.Contains(num) || num == medianValue) && result.Where(x => x == num).Count() < frequencyDict[num] && result.Count < length)
+                result.Add(num);
+
+        var firstAttempt = result.Count == length;
+
+        frequencyDict = array.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+
+        topFrequentNumbers = frequencyDict.Where(x => x.Key != medianValue).OrderByDescending(x => x.Value)
+                                            .ThenBy(x => array.IndexOf(x.Key))
+                                            .Select(x => x.Key)
+                                            .Take(distinctCount)
+                                            .ToList();
+        result = topFrequentNumbers;
+        foreach (var num in array)
+            if ((topFrequentNumbers.Contains(num) || num == medianValue) && result.Where(x => x == num).Count() < frequencyDict[num] && result.Count < length)
+                result.Add(num);
+
+        return firstAttempt || (result.Count == length);
+
+    }
+
+    public static void Main(string[] args)
+    {
+        List<int> arr = [1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6];
+        var result = GetMaxSubsequenceLen(arr);
+
+        Console.WriteLine("Result for arr: ");
+        foreach (var num in result)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
     }
 }
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//public class Solution
+//{
+//    public static List<int> GetMaxSubsequenceLen(List<int> arr)
+//    {
+//        List<int> sortedArr = new List<int>(arr);
+//        sortedArr.Sort();
+//        Dictionary<int, int> subsequenceLength = new Dictionary<int, int>();
+
+//        for (int i = 0; i < sortedArr.Count; i++)
+//        {
+//            int medianValue = sortedArr[i];
+//            List<int> leftSubArray = sortedArr.Take(i).ToList();
+//            List<int> rightSubArray = sortedArr.Skip(i + 1).ToList();
+//            List<int> medianList = arr.Where(x => x == medianValue).ToList();
+
+//            var leftDistinctCollection = new HashSet<int>(leftSubArray.Where(x => x != medianValue));
+//            var rightDistinctCollection = new HashSet<int>(rightSubArray.Where(x => x != medianValue));
+
+//            int length = medianList.Count % 2 == 0 ? medianList.Count - 1 : medianList.Count;
+
+//            if (rightDistinctCollection.Count == 0 || leftDistinctCollection.Count == 0)
+//            {
+//                UpdateSubsequenceLength(subsequenceLength, medianValue, length);
+//            }
+//            else if (leftSubArray.Count < rightSubArray.Count)
+//            {
+//                ProcessSubArray(rightSubArray, medianValue, leftDistinctCollection.Count, leftSubArray.Count, subsequenceLength, sortedArr[i]);
+//            }
+//            else
+//            {
+//                ProcessSubArray(leftSubArray, medianValue, rightDistinctCollection.Count, rightSubArray.Count, subsequenceLength, sortedArr[i]);
+//            }
+//        }
+
+//        List<int> finalResult = arr.Select(item => subsequenceLength.ContainsKey(item) ? subsequenceLength[item] : 0).ToList();
+//        return finalResult;
+//    }
+
+//    private static void UpdateSubsequenceLength(Dictionary<int, int> subsequenceLength, int key, int length)
+//    {
+//        if (subsequenceLength.TryGetValue(key, out int value))
+//        {
+//            if (value < length)
+//                subsequenceLength[key] = length;
+//        }
+//        else
+//            subsequenceLength.Add(key, length);
+//    }
+
+//    private static void ProcessSubArray(List<int> subArray, int medianValue, int distinctCount, int subArrayCount, Dictionary<int, int> subsequenceLength, int originalValue)
+//    {
+//        bool balancedArray = IsBalancedSubsequencePossible(subArray, medianValue, distinctCount, subArrayCount);
+//        if (balancedArray)
+//        {
+//            int length = subArrayCount * 2 + 1;
+//            length = length % 2 == 0 ? length - 1 : length;
+//            UpdateSubsequenceLength(subsequenceLength, originalValue, length);
+//        }
+//        else
+//        {
+//            int maxDistinct = distinctCount;
+//            while (maxDistinct > 0)
+//            {
+//                maxDistinct--;
+//                int allocation = CalculateMaxAllocation(subArray, maxDistinct);
+//                balancedArray = IsBalancedSubsequencePossible(subArray, medianValue, maxDistinct, allocation);
+//                if (balancedArray)
+//                {
+//                    int length = allocation * 2 + 1;
+//                    length = length % 2 == 0 ? length - 1 : length;
+//                    UpdateSubsequenceLength(subsequenceLength, originalValue, length);
+//                    break;
+//                }
+//            }
+//        }
+//    }
+
+//    private static int CalculateMaxAllocation(List<int> array, int distinctCount)
+//    {
+//        var frequencyDict = array.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+//        return frequencyDict.OrderByDescending(x => x.Value).Take(distinctCount).Sum(x => x.Key);
+//    }
+
+//    private static bool IsBalancedSubsequencePossible(List<int> array, int medianValue, int distinctCount, int length)
+//    {
+//        var frequencyDict = array.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+//        var topFrequentNumbers = frequencyDict.Where(x => x.Key != medianValue).OrderByDescending(x => x.Value).Select(x => x.Key).Take(distinctCount).ToList();
+//        var result = new List<int>(topFrequentNumbers);
+
+//        foreach (var num in array)
+//            if ((topFrequentNumbers.Contains(num) || num == medianValue) && result.Count(x => x == num) < frequencyDict[num] && result.Count < length)
+//                result.Add(num);
+
+//        return result.Count == length;
+//    }
+
+//    public static void Main(string[] args)
+//    {
+//        List<int> arr = new List<int> { 1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6 };
+//        var result = GetMaxSubsequenceLen(arr);
+
+//        Console.WriteLine("Result for arr: ");
+//        foreach (var num in result)
+//        {
+//            Console.Write(num + " ");
+//        }
+//        Console.WriteLine();
+//    }
+//}
---
+++ Subsequence/Readme.md
@@ -0,0 +1,107 @@
+﻿**Median Analysis**
+
+Given an array `arr` of n integers, for each index i, find the length of the longest odd-length subsequence of the array
+such that `arr[i]` is the median of the array and the number of distinct elements in the subsequence less than `arr[i]`
+is equal to the number of distinct elements in the subsequence greater than `arr[i]`.
+
+Note: A subsequence of an array is defined as an array obtained after deleting zero or more elements of the given array.
+Also, the median of an array with an odd number of elements is defined as the middle element obtained when the array is sorted.
+
+**Example**
+
+Suppose n = 8 and `arr` = [3, 4, 1, 5, 1, 2, 2, 2].
+
+For 3, the optimal subsequence is [3, 4, 1, 5, 2] which has a median of 3.
+For 4, the optimal subsequence is [3, 4, 5] which has a median of 5.
+For 1, the optimal subsequence is [1]. Note that [1, 1] cannot be chosen as the subsequence must be of odd length.
+For 5, the optimal subsequence is [5].
+For 2, the optimal subsequence is [3, 1, 1, 2, 2].
+Thus the answer is [5, 3, 1, 1, 1, 5, 5, 5].
+
+**Function Description**
+
+Complete the function `getMaxSubsequenceLen` in the editor below.
+
+`getMaxSubsequenceLen` has the following parameter:
+
+- `int arr[n]`: The input array
+
+Returns:
+- `int[n]`: The length of the longest subsequence for each element respecting the given conditions
+
+**Constraints**
+
+\[1 ≤ n ≤ 10^5\]
+\[1 ≤ arr[i] ≤ 10^9\]
+
+**Input Format For Custom Testing**
+
+The first line contains an integer n, which denotes the number of elements in array `arr`.
+
+Each line i of the n subsequent lines (where 0 < i < n) contains an integer that denotes `arr[i]`.
+
+**Sample Case 0**
+**Sample Input For Custom Testing**
+
+STDIN       FUNCTION
+-----       --------
+5           → arr[] size n = 5
+1           → arr[] = [1, 3, 2, 1, 3]
+3
+2
+1
+3
+
+**Sample Output**
+
+1
+1
+5
+1
+1
+
+**Explanation**
+
+For 1 and 3, the optimal subsequences are [1] and [3] respectively. For 2, the entire array is a valid subsequence.
+
+**Sample Case 1**
+**Sample Input For Custom Testing**
+
+STDIN       FUNCTION
+-----       --------
+12          → arr[] size n = 12
+1           → arr[] = [1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6]
+2
+2
+3
+4
+4
+4
+5
+6
+6
+6
+6
+
+**Sample Output**
+
+1
+5
+5
+7
+11
+11
+11
+7
+3
+3
+3
+3
+
+**Explanation**
+
+For 1 the optimal subsequence is [1]. For 2, the optimal subsequence is [1, 2, 2, 3, 4]. For 3, the optimal subsequence is [1, 2, 2, 3, 4, 5, 6]. Note that [1, 2, 2, 3, 4, 4, 4] is not a valid subsequence as the distinct number of elements less than 3 i.e. 2 is not equal to that greater than 3 i.e.1.
+
+For 4 the optimal subsequence is [1, 2, 2, 4, 4, 4, 5, 6, 6, 6, 6]. Note that the entire array is not a valid subsequence for 4 as the number of distinct elements less than 4 is 3 but that greater than 4 is only 2.
+
+For 5 the optimal subsequence is [4, 5, 6]. For 6 the optimal subsequence is [6, 6, 6].
