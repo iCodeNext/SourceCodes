2da17e5ca56fa834a8e2e6f4dd108ffda3e3f304
mohammadKarimi <mha.karimi@gmail.com>
2024-06-11 06:00:39 +0330

    add some performance tips

---
+++ EFPerformance/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFPerformance;
+internal class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer("Server=DESKTOP-TVCSFN3\\MHA;Database=EFCoreExamples;Trusted_Connection=True;Encrypt=false")
+            .EnableSensitiveDataLogging()
+            .LogTo(Console.WriteLine, LogLevel.Information);
+    }
+
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+
+    public ICollection<Tag> Tags { get; set; }
+}
+
+public class Tag
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    public int PostId { get; set; }
+    [ForeignKey(nameof(PostId))]
+    public Post Post { get; set; }
+}
--- EFPerformance/Program.cs
+++ EFPerformance/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFPerformance;
+using Microsoft.EntityFrameworkCore;
+
+AppDbContext _context = new();
+
+#region Project only
+
+//foreach (var post in _context.Posts)
+//{
+//    Console.WriteLine("Post: " + post.Title);
+//}
+
+//foreach (var postTitle in _context.Posts.Select(x => x.Title))
+//{
+//    Console.WriteLine("Post: " + postTitle);
+//} 
+#endregion
+
+
+#region resultset size
+//var posts = _context.Posts
+//    .Where(p => p.Title.StartsWith("P"))
+//    .ToList();
+
+
+//var posts = _context.Posts
+//    .Where(p => p.Title.StartsWith("P"))
+//    .Take(20)
+//    .ToList();
+#endregion
+
+#region Cartesian explosion
+
+//var posts = _context.Users.Include(x => x.Posts).ToList();
+//var posts = _context.Users.Include(x => x.Posts).AsSplitQuery().ToList();
+#endregion
+
+
+
+
+Console.ReadLine();
