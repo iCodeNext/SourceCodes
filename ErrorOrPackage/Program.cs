da9e1bdca276d0508eeda51303bdf4febfaa0494
mohammadKarimi <mha.karimi@gmail.com>
2024-07-02 15:25:24 +0330

    Add error or project

---
+++ ErrorOrPackage/Program.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+
+ErrorOr<float> Divide(int a, int b)
+{
+    if (b is 0)
+    {
+        return Error.Unexpected(description: "Cannot divide by zero");
+    }
+
+    return a / b;
+}
+
+
+var result = Divide(4, 0);
+if (result.IsError)
+{
+    Console.WriteLine(result.FirstError.Description);
+    return;
+}
+Console.WriteLine(result.Value * 2); // 4
+
