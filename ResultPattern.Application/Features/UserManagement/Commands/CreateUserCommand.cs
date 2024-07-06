18c06dd719f6cc3629c854bd1a455e093454d7de
mohammadKarimi <mha.karimi@gmail.com>
2024-07-06 03:56:45 +0330

    Add application

---
+++ ResultPattern.Application/Features/UserManagement/Commands/CreateUserCommand.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+
+namespace ResultPattern.Application.Features.UserManagement.Commands;
+public record CreateUserCommand(int Id, string Name, string Email) : IRequest<bool>;
---
+++ ResultPattern.Application/Features/UserManagement/Commands/CreateUserCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultPattern.Application.Features.UserManagement.Commands;
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, bool>
+{
+    public CreateUserCommandHandler()
+    {
+    }
+
+    public Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        
+    }
+}
