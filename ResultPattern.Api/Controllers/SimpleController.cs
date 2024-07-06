f2e3772c20ab34ea9896d06dea484074631e97df
mohammadKarimi <mha.karimi@gmail.com>
2024-07-06 03:56:38 +0330

    Add api

---
+++ ResultPattern.Api/Controllers/SimpleController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResultPattern.Api;
+
+[ApiController]
+public class SimpleController(ISender sender) : Controller
+{
+    private readonly ISender _sender = sender;
+
+    public IActionResult Add()
+
+}
