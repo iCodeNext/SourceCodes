7988fb7f1fd2c6a190791e36532f6c9b0a4b2c01
mohammadKarimi <mha.karimi@gmail.com>
2024-07-06 04:24:39 +0330

    Complete api

--- ResultPattern.Api/Controllers/SimpleController.cs
+++ ResultPattern.Api/Controllers/SimpleController.cs
@@ -1,13 +1,18 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using ResultPattern.Application.Features.UserManagement;
 
 namespace ResultPattern.Api;
 
 [ApiController]
+[Route("api/[controller]/[action]")]
 public class SimpleController(ISender sender) : Controller
 {
     private readonly ISender _sender = sender;
 
-    public IActionResult Add()
-
+    [HttpPost]
+    public async Task<IActionResult> Add(CreateUserCommand command)
+    {
+        return Ok(await _sender.Send(command));
+    }
 }
--- ResultPattern.Api/Program.cs
+++ ResultPattern.Api/Program.cs
@@ -1,5 +1,13 @@
+using ResultPattern.Application;
+using ResultPattern.Application.Common.Interfaces;
+using ResultPattern.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<INotificationService, NotificationService>();
+builder.Services.RegisterApplicationServices();
+
 var app = builder.Build();
 app.MapControllers();
 app.Run();
