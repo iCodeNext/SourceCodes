eb46eff89f911a65e5b72b2b1a186d02ab9cbf93
mohammadKarimi <mha.karimi@gmail.com>
2024-07-30 08:11:18 +0330

    Add Project

--- ConfigurationName/Controllers/WeatherForecastController.cs
+++ ConfigurationName/Controllers/WeatherForecastController.cs
@@ -2,30 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ConfigurationName.Controllers;
 [ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+[Route("[controller]/[action]")]
+public class Home : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    public IActionResult Index()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return Ok("Hello From iCodeNext!");
     }
 }
