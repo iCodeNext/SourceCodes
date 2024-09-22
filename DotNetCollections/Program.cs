44f3243062f752e4f478c6b74cad9db9a955ff33
mohammadKarimi <mha.karimi@gmail.com>
2024-09-22 09:59:35 +0330

    Complete List vs ArrayList.

--- DotNetCollections/Program.cs
+++ DotNetCollections/Program.cs
@@ -1,16 +1,29 @@
-﻿//using System.Collections;
+﻿using System.Collections;
+
+//int[] ints = new int[4];
+//int[] ints2 = new int[8];
+
 
 //ArrayList arrayList = new ArrayList();
 //arrayList.Add(1);
-//arrayList.Add("MK");
-//arrayList.Add(true);
+//arrayList.Add(6);
+//arrayList.Add(3);
+//arrayList.Add(2);
+
+////arrayList.BinarySearch
 
 //foreach (var item in arrayList)
 //{
+
 //    Console.WriteLine(item);
 //}
 
 
+//List<int> arrayList = new List<int>();
+
+//arrayList.Add(1);
+//arrayList.Add(3);
+//arrayList.Add(2);
 
 
 
@@ -25,16 +38,14 @@
 
 
 
+ 
+using BenchmarkDotNet.Running;
+using DotNetCollections;
 
-
-
-//using BenchmarkDotNet.Running;
-//using DotNetCollections;
-
-//public class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        BenchmarkRunner.Run<ListVsArrayListBenchmark>();
-//    }
-//}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<ListVsArrayListBenchmark>();
+    }
+}
