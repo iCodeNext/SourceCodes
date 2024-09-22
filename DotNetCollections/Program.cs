8d497e8fa060570e2ba5f9312252ef264c4d731d
mohammadKarimi <mha.karimi@gmail.com>
2024-09-22 08:14:49 +0330

    Add Benchmark for List and ArrayList.

--- DotNetCollections/Program.cs
+++ DotNetCollections/Program.cs
@@ -1,111 +1,40 @@
-﻿List<int> ints = new List<int>();
+﻿//using System.Collections;
 
-foreach (var item in ints)
-{
-    Console.WriteLine(item);
-}
+//ArrayList arrayList = new ArrayList();
+//arrayList.Add(1);
+//arrayList.Add("MK");
+//arrayList.Add(true);
 
-//var enumerator = myList.GetEnumerator();
-//while (enumerator.MoveNext())
+//foreach (var item in arrayList)
 //{
-//    var value = enumerator.Current;
-//    Console.WriteLine(value);
-//}
-
-public interface IEnumerable
-{
-    IEnumerator GetEnumerator();
-}
-public interface IEnumerable<T> : IEnumerable
-{
-    IEnumerator<T> GetEnumerator();
-}
-
-public interface IEnumerator
-{
-    bool MoveNext();
-    object Current { get; }
-    void Reset();
-}
-public interface IEnumerator<T> : IEnumerator, IDisposable
-{
-    T Current { get; }
-}
-
-public interface ICollection : IEnumerable
-{
-    int Count { get; }
-    void CopyTo(Array array, int index);
-}
-
-public interface IList : ICollection, IEnumerable
-{
-    object? this[int index] { get; set; }
-    bool IsFixedSize { get; }
-    bool IsReadOnly { get; }
-    int Add(object? value);
-    void Clear();
-    bool Contains(object? value);
-    int IndexOf(object? value);
-    void Insert(int index, object? value);
-    void Remove(object? value);
-    void RemoveAt(int index);
-}
-
-
-public interface ICollection<T> : IEnumerable<T>, IEnumerable
-{
-    int Count { get; }
-    bool IsReadOnly { get; }
-    void Add(T item);
-    void Clear();
-    bool Contains(T item);
-    void CopyTo(T[] array, int arrayIndex);
-    bool Remove(T item);
-}
-
-public interface IList<T> : ICollection<T>, IEnumerable<T>, IEnumerable
-{
-    T this[int index] { get; set; }
-    int IndexOf(T item);
-    void Insert(int index, T item);
-    void RemoveAt(int index);
-}
-
-public class ICodeNextCollection<T> : IReadOnlyList<T>
-{
-    public T this[int index] => throw new NotImplementedException();
-
-    public int Count => throw new NotImplementedException();
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-
-//ICodeNextCollection<int> collections = new ICodeNextCollection<int>();
-
-//if (!collections.IsReadOnly)
-//{
-//    collections.Add(5);
+//    Console.WriteLine(item);
 //}
 
 
-//public interface IReadOnlyCollection<out T> : IEnumerable<T>, IEnumerable
-//{
-//    int Count { get; }
-//}
 
-//public interface IReadOnlyList<out T> : IReadOnlyCollection<T>,
-//                                        IEnumerable<T>, IEnumerable
-//{
-//    T this[int index] { get; }
-//}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using BenchmarkDotNet.Running;
+//using DotNetCollections;
+
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        BenchmarkRunner.Run<ListVsArrayListBenchmark>();
+//    }
+//}
---
+++ DotNetCollections/benchmarks/BenchmarkArrayListVsList.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DotNetCollections;
+
+[MemoryDiagnoser]
+public class ListVsArrayListBenchmark
+{
+    private const int NumberOfElements = 10000;
+
+    [Benchmark]
+    public void ArrayList_Add()
+    {
+        var arrayList = new ArrayList();
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            arrayList.Add(i);
+        }
+    }
+
+    [Benchmark]
+    public void List_Add()
+    {
+        var list = new List<int>();
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            list.Add(i);
+        }
+    }
+
+    [Benchmark]
+    public void ArrayList_Access()
+    {
+        var arrayList = new ArrayList();
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            arrayList.Add(i);
+        }
+
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            int value = (int)arrayList[i];
+        }
+    }
+
+    [Benchmark]
+    public void List_Access()
+    {
+        var list = new List<int>();
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            list.Add(i);
+        }
+
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            int value = list[i];
+        }
+    }
+}
